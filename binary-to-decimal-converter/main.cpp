9df58e92feb1d82faf9b4bd517ade103d253d1d1
Tadeo Bennett <tadeos.bennett@gmail.com>
2023-02-06 23:09:22 -0600

    complete the binary to decimal converter

--- binary-to-decimal-converter/main.cpp
+++ binary-to-decimal-converter/main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main(){
+int main()
+{
+    string input;
+    cout << "Enter the binary string: ";
+    getline(cin, input);
+
+    int value = 0;
+    int exponentValue = 1;
+    for (int i = input.length() - 1; i >= 0; i--)
+    {
+        if (input[i] == '0')
+        {
+            // do nothing;
+        }
+        else if (input[i] == '1')
+        {
+            value += exponentValue;
+        }
+        else
+        {
+            cout << "Invalid bit string";
+            break;
+        }
+        // cout<<"exponent value: "<<exponentValue<<endl;
+        exponentValue *= 2;
+    }
+    cout << "\nValue is " << value;
 
     return 0;
 }
